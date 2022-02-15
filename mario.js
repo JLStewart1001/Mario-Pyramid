a9fec5424653fab5054cae83d0293f52cb5b0d14
Johnathan Stewart <98654889+JLStewart1001@users.noreply.github.com>
2022-02-15 00:02:12 +0000

    allow user to input pyramid height and respond to button click

--- mario.js
+++ mario.js
@@ -1,16 +1,22 @@
-
-let x = prompt("Enter a number")
-let string = ""
-for (let i = 1; i <= x; i++) {
-  for (let j = 0; j < x - i; j++) {
-    string += " . ";
+const element = document.getElementById("button");
+function myFunction() {
+  const x = document.getElementById("heightNum").value;
+  let string = ""
+  for (let i = 1; i <= x; i++) {
+    for (let j = 0; j < x - i; j++) {
+      string += " . ";
+    }
+    for (let k = 0; k < i; k++) {
+      string += "#";
+     }
+     string += "\n";
   }
-  for (let k = 0; k < i; k++) {
-    string += "#";
-  }
-  string += "\n";
+ rowStr.innerText = string;
+ document.body.appendChild(rowStr);
+ const element = document.getElementById("button");
+ element.addEventListener("click", secFunction);
+ function secFunction() {
+   var myobj = document.getElementById("rowStr");
+   myobj.remove();
+ }
 }
-
-var rowStr = document.createElement("p");
-rowStr.innerText = string;
-document.body.appendChild(rowStr);
